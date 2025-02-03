45431bf332806cec5deb83a35adfae095d866513
Dmitriy Ivanov <dmtr_ivanov@outlook.com>
2025-02-03 09:38:38 +0300

    added func mask_account_card, get_date

--- src/widget.py
+++ src/widget.py
@@ -1,30 +1,39 @@
 import re
+import datetime
+
 
 def mask_account_card(user_input: str) -> str:
+    """функция маскирует номер карты"""
 
-    numbers = re.findall(r'\d+', user_input)
+    numbers = re.findall(r"\d+", user_input)
     if not numbers:
-        return 'please enter correct information'
+        return "please enter correct information"
 
     card_number = numbers[0]
 
-    if re.search('visa', user_input, re.IGNORECASE):
-        num_cart_visa = f'Visa Platinum {card_number[:4]} {card_number[4:6]}** **** {card_number[12:]}'
-        return num_cart_visa
 
-    elif re.search('maestro', user_input, re.IGNORECASE):
-        num_cart_maestro = f'Maestro {card_number[:4]} {card_number[4:6]}** **** {card_number[12:]}'
-        return num_cart_maestro
+    card_types = {
+        "Classic": "Visa Classic",
+        "Platinum": "Visa Platinum",
+        "Gold": "Visa Gold",
+        "Maestro": "Maestro",
+        "Master": "MasterCard",
+    }
 
-    elif re.search('[а-яА-Я]', user_input):
-        num_account = f'Счет **{user_input[-4:]}'
-        return num_account
+    for keyword, card_type in card_types.items():
+        if re.search(keyword, user_input, re.IGNORECASE):
+            return f"{card_type} {card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
 
-    else:
-        error_message = 'please enter correct information'
-        return error_message
-
-# Пример использования
-print(mask_account_card('Visa Platinum 7000792289606361'))
+    if re.search("[а-яА-Я]", user_input):
+        return f"Счет **{user_input[-4:]}"
 
 
+
+
+def get_date(my_date: str) -> str:
+    """функция приводит введенную дату к формату ДД.ММ.ГГГГ"""
+    date_obj = datetime.datetime.strptime(my_date, "%Y-%m-%dT%H:%M:%S.%f")
+    return date_obj.strftime("%d.%m.%Y")
+
+
+print(mask_account_card('Visa Gold 5999414228426353'))
