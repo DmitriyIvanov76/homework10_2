51f35b28aec57bce817aebeb44e9b1e526b33cd4
Dmitriy Ivanov <dmtr_ivanov@outlook.com>
2025-03-18 19:22:58 +0300

    added logger

--- src/masks.py
+++ src/masks.py
@@ -1,17 +1,35 @@
+import logging
+
+# настройка логгера для модуля
+logger = logging.getLogger("masks")
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler("logs/masks.log", mode="w", encoding="UTF-8")
+file_formater = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formater)
+logger.addHandler(file_handler)
+
+
 def get_mask_card_number(number_cart: str) -> str:
     """функция принимает на вход номера карты, а возвращает номер с несколькими цифрами закрытыми звездочкой"""
 
+    logger.info("проверка карты соответствию заданной длине")
     if len(number_cart) == 16 and number_cart.isdigit():
+        logger.info("вывод информации")
         return f"{number_cart[0:4]} {number_cart[5:7]}** **** {number_cart[-4:]}"
+
     else:
+        logger.error("номер карты не соответствует заданной длине")
         raise ValueError("Ошибка ввода номера карты")
 
 
 def get_mask_account(number_account: str) -> str:
-    """функция принимает на вход номер счета, а возвращает последние четыре цифры счета"""
+    """функция принимает на вход номер счета,а возвращает последние четыре цифры счета"""
 
+    logger.info("проверка номера счета соответствию заданной длине")
     if len(number_account) == 20 and number_account.isdigit():
+        logger.info("вывод информации")
         coded_account_number = "**" + number_account[-4:]
         return coded_account_number
     else:
+        logger.error("Ошибка ввода номера счета")
         raise ValueError("ошибка ввода номера счета")
--- src/utils.py
+++ src/utils.py
@@ -1,15 +1,28 @@
 import json
+import logging
 from json import JSONDecodeError
 
 
 def convertor_json(path: str) -> list[dict]:
     """функция принимает на вход путь до файла
     json, возвращает список словарей"""
+
+    # настройка логгера для модуля
+    logger = logging.getLogger("utils")
+    logger.setLevel(logging.DEBUG)
+    file_handler = logging.FileHandler("logs/utils.log", mode="w", encoding="UTF-8")
+    file_formater = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
+    file_handler.setFormatter(file_formater)
+    logger.addHandler(file_handler)
     try:
+        logger.info("Начало работы")
         with open(path, encoding="UTF-8") as file:
             content = json.load(file)
+            logger.info("вывод результата")
             return content
     except FileNotFoundError:
+        logger.error("Файл по указанному пути - отсутствует")
         return []
     except JSONDecodeError:
+        logger.error("Файл не содержит информации для обработки")
         return []
