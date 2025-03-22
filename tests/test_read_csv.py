ddb1f6c7ef9073ba9b406ae5b4e9819dd2d8fd10
Dmitriy Ivanov <dmtr_ivanov@outlook.com>
2025-03-22 09:54:32 +0300

    homework13_1 -release

--- tests/test_read_csv.py
+++ tests/test_read_csv.py
@@ -1,8 +1,10 @@
 from unittest.mock import mock_open, patch
+
 from src.read_csv import csv_reader
 
-@patch('builtins.open', new_callable=mock_open, read_data='key,value\nkey1,value1\n')
+
+@patch("builtins.open", new_callable=mock_open, read_data="key,value\nvalue1,value2\n")
 def test_read_csv(mock_file):
-    result = csv_reader('test.csv')
-    assert result == [{'key': 'value1', 'value': 'value2'}]
-    mock_file.assert_called_once_with('test.csv', newline='' ,encoding='UTF-8')
+    result = csv_reader("test.csv")
+    assert result == [{"key": "value1", "value": "value2"}]
+    mock_file.assert_called_once_with("test.csv", newline="", encoding="UTF-8")
